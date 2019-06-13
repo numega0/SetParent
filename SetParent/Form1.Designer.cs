0d8388858e86f2623a1c8af46a52d1cbf6b1e886
numega0 <alone_wolf_2758@hotmail.com>
2019-06-14 02:02:42 +0300

    Add project files.

---
+++ SetParent/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SetParent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb1.Location = new System.Drawing.Point(0, 0);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(850, 529);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb1.TabIndex = 1;
+            this.pb1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 529);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pb1;
+    }
+}
+
---
+++ SetParent/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+
+namespace SetParent
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        private static extern int FindWindow(string sClass, string sWindow);
+
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter,
+        int x, int y, int width, int height, uint uFlags);
+
+        private const uint SHOWWINDOW = 0x0040;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resizeWow()
+        {
+            System.Diagnostics.Process[] itunesProcesses =
+                System.Diagnostics.Process.GetProcessesByName("Wow");
+
+            if (itunesProcesses.Length > 0)
+            {
+                SetWindowPos(itunesProcesses[0].MainWindowHandle, this.Handle,
+                    0, 0, pb1.Width,
+                    pb1.Height, SHOWWINDOW);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nWinHandle = FindWindow(null, "World of Warcraft");
+
+            if (nWinHandle == 0)
+            {
+                MessageBox.Show("HATA");
+            }
+            else {
+
+                resizeWow();
+                SetParent((IntPtr)nWinHandle, pb1.Handle);
+            }
+
+        }
+    }
+}
